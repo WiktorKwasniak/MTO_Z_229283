e59e015d3908df4daf313929fab8e3b450b26bde
WiktorKwasniak <229283@edu.p.lodz.pl>
2022-10-09 11:35:58 +0200

    minor refactor of my_printf function - moved lower/upper case changing logic to separate function

--- lab2/main.c
+++ lab2/main.c
@@ -2,22 +2,54 @@
 #include <string.h>
 #include <ctype.h>
 
+char* change_case(char *param) {
+	char tmp_buf[1024];
+	for (int j = 0; j < strlen(param); j++) {
+		if (param[j] >= 'A' && param[j] <= 'Z') {
+			tmp_buf[j] = tolower(param[j]);
+		} else {
+			tmp_buf[j] = toupper(param[j]);
+		}
+	}
+
+	return tmp_buf;
+}
+
 int my_printf(char *format_string, char *param){
 	for(int i=0;i<strlen(format_string);i++){
 		if((format_string[i] == '#') && (format_string[i+1] == 'k')){
 			i++;
 
-			char tmp_buf[1024];
-			for (int j = 0; j < strlen(param); j++) {
-				if (param[j] >= 'A' && param[j] <= 'Z') {
-					tmp_buf[j] = tolower(param[j]);
-				} else {
-					tmp_buf[j] = toupper(param[j]);
+			char *buff = change_case(param);
+			printf("%s", buff);
+		} else if ((format_string[i] == '#') && (format_string[++i] == '.')) {
+			i++;
+			int k = i;
+			int init_k = i;
+			while (isdigit(format_string[k])) {
+				k++;
+			}
+
+			int p_length = 0;
+			if (format_string[k] == 'k') {
+				int d = 1;
+				for (int rk = --k; rk >= init_k; rk--) {
+					p_length += format_string[rk]*d;
+					d *= 10;
+				}
+
+				change_case(param);
+
+				while () {
+					
 				}
 			}
 
-			printf("%s", tmp_buf);
-		}else
+			for (int k = i; k < strlen(format_string); k++) {
+				if (k)
+			}
+ 			(format_string[i] == '#') && (format_string[i+1] == '')  && (format_string[++i] == 'k')
+		} else
 			putchar(format_string[i]);
 	}
 	puts("");
