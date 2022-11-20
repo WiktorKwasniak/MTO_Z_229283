4983cf004a3ca320a6eab3a2d51753ba04d7085c
Wiktor Kwaśniak <wiktor.kwasniak@beefee.co.uk>
2022-11-20 11:07:47 +0100

    added #g functionality

--- lab5/main.py
+++ lab5/main.py
@@ -3,20 +3,32 @@
 import sys
 
 def my_printf(format_string,param):
-    #print(format_string)
     shouldDo=True
     for idx in range(0,len(format_string)):
         if shouldDo:
-            if format_string[idx] == '#' and format_string[idx+1] == 'k':
-                print(param,end="")
-                shouldDo=False
+            if format_string[idx] == '#' and format_string[idx+1] == 'g' and format_string[idx-1] != '#':
+                if param.isnumeric():
+                    new_str = ''
+                    for idy in range(0,len(param)):
+                        print(param[idy])
+                        if not param[idy] == '0':
+                            new_str += str(int(param[idy])-1)
+                        else:
+                            new_str += '9'
+
+                    print(new_str, end="")
+                    shouldDo=False
+                else:
+                    print(format_string[idx],end="")
+
             else:
                 print(format_string[idx],end="")
+
         else:
             shouldDo=True
+
     print("")
 
 data=sys.stdin.readlines()
-
 for i in range(0,len(data),2):
     my_printf(data[i].rstrip(),data[i+1].rstrip())
