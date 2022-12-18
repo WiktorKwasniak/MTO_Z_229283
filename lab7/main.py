d69de80b3b10fcd2e2388fc8cb0cee9b84f9c827
WiktorKwasniak <229283@edu.p.lodz.pl>
2022-12-18 11:14:32 +0100

    Refactor code to make it more clean

--- lab7/main.py
+++ lab7/main.py
@@ -2,38 +2,35 @@
 
 import sys
 
+
 def convertHex(string):
     converted = ''
     for char in string:
         if 'a' <= char <= 'f':
-            converted += chr(ord(char)+6)
-        else:
-            converted += char
+            char = chr(ord(char)+6)
+ 
+        converted += char
 
     return converted
 
 
 def my_printf(format_string, param):
-    #print(format_string)
     shouldDo = True
     for idx in range(0, len(format_string)):
         if shouldDo:
-            if format_string[idx] == '#' and format_string[idx+1] == 'j':
-                if param.isnumeric():
-                    hexValue = f"{int(param):x}"
-                    convertedStr = convertHex(hexValue)
-                    print(f'\n{convertedStr}')
-                    print(hexValue, end="")
-                    shouldDo = False
-                else:
-                    print(format_string[idx], end="")
+            if format_string[idx] == '#' and format_string[idx+1] == 'j' and param.isnumeric():
+                hexValue = f"{int(param):x}"
+                print(convertHex(hexValue), end="")
+
+                shouldDo = False
             else:
                 print(format_string[idx], end="")
         else:
             shouldDo = True
     print("")
 
-data=sys.stdin.readlines()
+
+data = sys.stdin.readlines()
 
 for i in range(0, len(data), 2):
     my_printf(data[i].rstrip(), data[i+1].rstrip())
