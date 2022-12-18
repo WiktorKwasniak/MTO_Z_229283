b82052662f8b158ed6044daad82417b18d13c414
WiktorKwasniak <229283@edu.p.lodz.pl>
2022-12-18 10:43:34 +0100

    Create print function to display hex number

--- lab7/main.py
+++ lab7/main.py
@@ -8,8 +8,11 @@ def my_printf(format_string, param):
     for idx in range(0, len(format_string)):
         if shouldDo:
             if format_string[idx] == '#' and format_string[idx+1] == 'j':
-                print("0x{:X}".format(param), end="")
-                shouldDo = False
+                if param.isnumeric():
+                    print(f"{int(param):x}", end="")
+                    shouldDo = False
+                else:
+                    print(format_string[idx], end="")
             else:
                 print(format_string[idx], end="")
         else:
