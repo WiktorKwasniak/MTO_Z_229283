d8d9c09bd496c5f7a54d859bb2f0f7ff481e722d
WiktorKwasniak <229283@edu.p.lodz.pl>
2022-12-18 10:37:17 +0100

    Change #k to #j and prepare initial tests

--- lab7/main.py
+++ lab7/main.py
@@ -2,21 +2,21 @@
 
 import sys
 
-def my_printf(format_string,param):
+def my_printf(format_string, param):
     #print(format_string)
-    shouldDo=True
-    for idx in range(0,len(format_string)):
+    shouldDo = True
+    for idx in range(0, len(format_string)):
         if shouldDo:
-            if format_string[idx] == '#' and format_string[idx+1] == 'k':
-                print(param,end="")
-                shouldDo=False
+            if format_string[idx] == '#' and format_string[idx+1] == 'j':
+                print("0x{:X}".format(param), end="")
+                shouldDo = False
             else:
-                print(format_string[idx],end="")
+                print(format_string[idx], end="")
         else:
-            shouldDo=True
+            shouldDo = True
     print("")
 
 data=sys.stdin.readlines()
 
-for i in range(0,len(data),2):
-    my_printf(data[i].rstrip(),data[i+1].rstrip())
+for i in range(0, len(data), 2):
+    my_printf(data[i].rstrip(), data[i+1].rstrip())
